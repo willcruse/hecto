c9191c8129d1ab5aee45b7ccb528b451d56d9266
Will Cruse <willcruse123@proton.me>
2023-01-10 19:01:10 +0000

    chapter 3

--- src/editor.rs
+++ src/editor.rs
@@ -1,45 +1,129 @@
-use std::io::{self, stdout};
-use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+use crate::Terminal;
+
+use termion::event::Key;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
 
 fn die(e: std::io::Error) {
+    Terminal::clear_screen();
     panic!("{e}");
 }
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
-}
-
 pub struct Editor {
-    should_quit: bool
+    should_quit: bool,
+    terminal: Terminal,
+    cursor_position: Position,
 }
 
 impl Editor {
     pub fn default() -> Editor {
-        Self { should_quit: false }
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialise terminal"),
+            cursor_position: Position { x: 0, y: 0 },
+        }
     }
 
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
-
         loop {
-            if let Err(error) = self.process_keypress() {
+            if let Err(error) = self.refresh_screen() {
                 die(error);
             }
 
             if self.should_quit {
                 break;
             }
+
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
         }
     }
 
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::cursor_hide();
+        Terminal::clear_screen();
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
+
+        if self.should_quit {
+            Terminal::clear_screen();
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(&self.cursor_position);
+        }
+
+        Terminal::cursor_show();
+        Terminal::flush()
+    }
+
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto Editor -- Version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }
+
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height - 1;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!(">\r");
+            }
+        }
+    }
+
+    fn move_cursor(&mut self, key: Key) {
+        let Position { mut x, mut y } = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height as usize;
+        let width = size.width as usize;
+
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y < height {
+                    y = y.saturating_add(1);
+                }
+            }
+            Key::Left => x = x.saturating_sub(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1)
+                }
+            },
+            Key::PageUp => y = 0,
+            Key::PageDown => y = height,
+            Key::Home =>  x = 0,
+            Key::End => x = width,
+            _ => (),
+        }
+        self.cursor_position = Position { x, y };
+    }
+
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Up 
+            | Key::Down 
+            | Key::Left 
+            | Key::Right
+            | Key::PageUp
+            | Key::PageDown
+            | Key::End
+            | Key::Home => self.move_cursor(pressed_key),
             _ => (),
         }
         Ok(())
